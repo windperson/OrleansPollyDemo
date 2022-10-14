918b4b65d141c1c3bf1b369ffbc024368c8cf060
Isak Pao <windperson@gmail.com>
2022-10-14 18:04:52 +0800

    Command line polly demo

---
+++ src/ConsoleDemoPolly/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+using Polly;
+
+var retryPolicy = Policy.Handle<MyException>().Retry(3);
+
+int executionCount = 0;
+retryPolicy.Execute(() =>
+{
+    Console.WriteLine("\r\n===\r\nTry Polly in command line");
+    if (executionCount < 3)
+    {
+        Console.WriteLine("Next I will throw an exception");
+        executionCount++;
+        throw new MyException($"This is the {executionCount} time(s) exception");
+    }
+    Console.WriteLine("Oh Yes! We Passed!!!");
+});
+
+public class MyException : Exception
+{
+    public MyException(string message) : base(message)
+    {
+    }
+}
